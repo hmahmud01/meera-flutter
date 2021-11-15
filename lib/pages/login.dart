1d07a5a27864eaaa80341fc145d2c2a7227c4d51
Hasan Mahmud <hmahmud01@gmail.com>
2021-11-16 03:18:51 +0600

    login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -13,9 +14,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
-  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-  SharedPreferences preferences;
+  // final GoogleSignIn googleSignIn = new GoogleSignIn();
+  // final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  // SharedPreferences preferences;
   bool loading = false;
   bool isLoggedIn = false;
 
@@ -30,11 +31,12 @@ class _LoginState extends State<Login> {
       loading = true;
     });
 
-    preferences = await SharedPreferences.getInstance();
-    isLoggedIn = await googleSignIn.isSignedIn();
-    
-    if(isLoggedIn){
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (Context) => new HomePage()));
+    // preferences = await SharedPreferences.getInstance();
+    // isLoggedIn = await googleSignIn.isSignedIn();
+
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (Context) => new HomePage()));
     }
 
     setState(() {
@@ -42,24 +44,116 @@ class _LoginState extends State<Login> {
     });
   }
 
-  Future handleSignIn() async{
-    preferences = await SharedPreferences.getInstance();
+  Future handleSignIn() async {
+    // preferences = await SharedPreferences.getInstance();
 
     setState(() {
       loading = true;
     });
 
-    GoogleSignInAccount googleUser = await googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication = await googleUser.authentication;
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
+    // GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    // GoogleSignInAuthentication googleSignInAuthentication = await googleUser.authentication;
+    // final credential = GoogleAuthProvider.credential(
+    //   accessToken: googleSignInAuthentication.accessToken,
+    //   idToken: googleSignInAuthentication.idToken,
+    // );
     // User user = await firebaseAuth.signInWithCredential(credential);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+      body: Stack(
+        children: <Widget>[
+          // Center(
+          //   child: Image.asset('images/logo.png', fit: BoxFit.cover, width: 140.0,),
+          // ),
+          Container(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child: Image.asset("images/logo.png", width: 150.0,),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 200.0),
+            child: Center(
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(4.0),
+                          color: Colors.red.withOpacity(0.2),
+                          elevation: 0.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                icon: Icon(Icons.email),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(4.0),
+                          color: Colors.red.withOpacity(0.2),
+                          elevation: 0.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                icon: Icon(Icons.lock_outline),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(4.0),
+                          color: Colors.red,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: MaterialButton(onPressed: (){},
+                            minWidth: MediaQuery.of(context).size.width,
+                              child: Text("Login", textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                            ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                )
+              ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FlatButton(
+            color: Colors.red,
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (Context) => new HomePage()));
+            },
+            child: Text(
+              "Login with Google",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
